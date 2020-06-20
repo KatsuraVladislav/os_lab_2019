4bc71ea0e2607b9484ab93b82500ba38f0ffb51b
KatsuraVladislav <55664747+KatsuraVladislav@users.noreply.github.com>
2020-06-20 12:54:07 +0300

    Create Sum.c

---
+++ lab4/src/Sum.c
@@ -0,0 +1,16 @@
+#include "Sum.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Sum(int* array,int begin,int end) {
+  int sum = 0;
+  int i;
+  for(i=begin; i<=end;i++)
+  {
+      
+	sum+=array[i];
+        
+  }
+  return sum;
+}
